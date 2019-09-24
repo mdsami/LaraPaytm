61709a64b7ed1c136f2337a634cc700a50d308de
MD SAMI <mdsami.diu@gmail.com>
2019-09-25 00:01:37 +0600

    LaraPaytmServiceProvider aadde

---
+++ src/LaraPaytm.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MdSami\LaraPaytm;
+
+use Illuminate\Support\Manager;
+use Illuminate\Http\Request;
+
+class LaraPaytm extends Manager implements Contracts\Factory{
+
+	private $config;
+
+
+
+}
---
+++ src/LaraPaytmServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MdSami\LaraPaytm;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraPaytmServiceProvider extends ServiceProvider{
+
+	
+
+
+
+}
