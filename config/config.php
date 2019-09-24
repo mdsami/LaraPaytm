7f36ec1063925147400cf9670fa7cd834a2527fd
MD SAMI <mdsami.diu@gmail.com>
2019-09-24 23:52:20 +0600

    LaraPaytm

---
+++ config/config.php
@@ -0,0 +1,39 @@
+<?php
+    return array(
+        /*
+        |--------------------------------------------------------------------------
+        | Paytm Default Gateway
+        |--------------------------------------------------------------------------
+        | "sandbox" is for testing purpose
+        | "live" is for production 
+        |
+        */
+        'default'       => 'sandbox',
+        /*
+        |--------------------------------------------------------------------------
+        | Paytm Aditional settings
+        |--------------------------------------------------------------------------
+        |
+        */
+        'industry_type' => 'Retail',
+        'channel'       => 'WEB',
+        'order_prefix'  => 'EDU',
+        'website'       => 'your-website',
+        /*
+        |--------------------------------------------------------------------------
+        | Paytm Connection Credentials
+        |--------------------------------------------------------------------------
+        | sandbox,live
+        |
+        */
+        'connections' => [
+            'sandbox' => [
+                'merchant_key'  => 'your-merchant-key',
+                'merchant_mid'  => 'your-merchant-id',
+            ],
+            'live' => [
+                'merchant_key'  => 'your-merchant-key',
+                'merchant_mid'  => 'your-merchant-id',
+            ]
+        ],
+    );
